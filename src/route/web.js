21452c412043a49122ed8bc28f3862b6c7f90a4f
bui van son <Sonbui195304@gmail.com>
2025-11-12 15:24:29 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,17 @@
+import express from "express";
+let router =express.Router();
+let initWebRoutes =(app) =>{
+    router.get('/',(req,res) =>{
+        return res.send('Hello word with IT');
+    });
+    router.get('/hoi dan it ',(req,res) =>{
+        return res.send('hỏi dân it ');
+    });
+
+
+
+
+    return app.use("/",router);
+};
+
+module.exports =initWebRoutes;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine.js"; 
+import initWebRoutes from "./route/web.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+viewEngine(app); 
+initWebRoutes(app);
+
+const port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+  console.log(` Backend is running at http://localhost:${port}`);
+});
